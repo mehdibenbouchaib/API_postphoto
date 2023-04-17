d671a4b9b46f27adb9041d271006a667e601165e
mehdi <mehdi.benbouchaib@gmail.com>
2023-04-17 14:44:16 +0000

    add service and repo

---
+++ src/main/java/com/photo/service/AccountService.java
@@ -0,0 +1,33 @@
+package com.photo.service;
+
+import com.photo.model.AppUser;
+import com.photo.model.Role;
+
+import java.util.List;
+
+public interface AccountService {
+
+    public void saveUser(AppUser appUser);
+
+    public AppUser findByUsername(String username);
+
+    public AppUser findByEmail(String email);
+
+    public List<AppUser> userList();
+
+    public Role findUserRoleByName(String role);
+
+    public Role saveRole(Role role);
+
+    public void updateUser(AppUser appUser);
+
+    public AppUser findById(Long id);
+
+    public void deleteUser(AppUser appUser);
+
+    public void resetPassword(AppUser appUser);
+
+    public List<AppUser> getUserListByUsername(String username);
+
+    public void simpleSave(AppUser appUser);
+}
---
+++ src/main/java/com/photo/service/PostService.java
@@ -0,0 +1,25 @@
+package com.photo.service;
+
+import com.photo.model.AppUser;
+import com.photo.model.Post;
+import org.springframework.security.core.parameters.P;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.HashMap;
+import java.util.List;
+
+public interface PostService {
+
+    public Post savePost(AppUser appUser, HashMap<String, String> request, String postImageName);
+
+    public List<Post> postList();
+
+    public Post getPostById(Long id);
+
+    public List<Post> findPostByUsername(String username);
+
+    public Post deletPost(Post post);
+
+    public String savePostImage(HttpServletRequest request, String fileName);
+
+}
